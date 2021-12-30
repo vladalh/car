ab709aa16ad0ddef0da5ce7ba9ea3a5ccb6fbf77
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2021-12-30 22:46:16 +0300

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,76 +1,58 @@
 import csv
 import json
 
+
+def file_reader():
+    gap_file = []
+    with open(f"cars.csv", "r", encoding="utf-8") as file:
+        csv_reader = csv.reader(file, delimiter=";")
+        header = next(csv_reader)
+        for line in csv_reader:
+            gap_file.append(line)
+        return gap_file
+
+
+def file_processing(gap_file, country):
+    car_list = []
+    for line in gap_file:
+        if country in line:
+            car_list.append(line)
+            car_list.sort(key=lambda x: x[0])
+    return car_list
+
+
+def file_write(fileformat, country, car_list):
+    with open(f"file{country}.{fileformat}", "w", encoding="utf-8") as file:
+        for car in car_list:
+            car_tuple = tuple(car)
+            if fileformat == "txt":
+                car = " ".join(car) + "\n"
+                txt_file = file.write(car)
+            elif formatfile == "csv":
+                csv_file = csv.writer(file, dialect="excel")
+                csv_file.writerow(car_tuple)
+            elif formatfile == "json":
+                json_file = json.dump(car_list, file, indent=4)
+
+
 formatfile_list = ["txt", "csv", "json"]
 
+country_list = ["US", "Europe", "Japan"]
+
 formatfile = input("Enter the file format for saving data: txt, csv or json: ")
+
+country = input("Enter the name of the car manufacturer: US, Europe or Japan ")
 
 if formatfile not in formatfile_list:
     print("You entered the wrong file format")
     exit(0)
 
-car_us_list = []
-car_europe_list = []
-car_japan_list = []
+if country not in country_list:
+    print("You entered the wrong country of origin")
+    exit(0)
 
-with open(f"cars.csv", "r", encoding="utf-8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=";")
-    header = next(csv_reader)
 
-    for line in csv_reader:
-        if "US" in line:
-            car_us_list.append(line)
-            car_us_list.sort(key=lambda x: x[0])
-
-        elif "Europe" in line:
-            car_europe_list.append(line)
-            car_europe_list.sort(key=lambda x: x[0])
-
-        elif "Japan" in line:
-            car_japan_list.append(line)
-            car_japan_list.sort(key=lambda x: x[0])
-
-with open(f"fileus.{formatfile}", "w", encoding="utf-8") as cars_file:
-    for car in car_us_list:
-        car_us = tuple(car)
-        if formatfile == "txt":
-            car = " ".join(car) + "\n"
-            txt_file = cars_file.write(car)
-
-        elif formatfile == "csv":
-            csv_file = csv.writer(cars_file, dialect="excel")
-            csv_file.writerow(car_us)
-
-        elif formatfile == "json":
-            json_file = json.dump(car_us_list, cars_file, indent=4)
-
-with open(f"fileeurope.{formatfile}", "w", encoding="utf-8") as cars_file:
-    for car in car_europe_list:
-        car_europe = tuple(car)
-        if formatfile == "txt":
-            car = " ".join(car) + "\n"
-            cars_file.write(car)
-
-        elif formatfile == "csv":
-            csv_file = csv.writer(cars_file, dialect="excel")
-            csv_file.writerow(car_europe)
-
-        elif formatfile == "json":
-            json_file = json.dump(car_europe_list, cars_file, indent=4)
-
-with open(f"filejapan.{formatfile}", "w", encoding="utf-8") as cars_file:
-    for car in car_japan_list:
-        car_japan = tuple(car)
-        if formatfile == "txt":
-            car = " ".join(car) + "\n"
-            cars_file.write(car)
-
-        elif formatfile == "csv":
-            csv_file = csv.writer(cars_file, dialect="excel")
-            csv_file.writerow(car_japan)
-
-        elif formatfile == "json":
-            json_file = json.dump(car_japan_list, cars_file, indent=4)
+file_write(formatfile, country, file_processing(file_reader(), country))
 
 # vladalh@mail.ru
 
