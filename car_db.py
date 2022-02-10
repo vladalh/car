f65edfcb134b93eda5533292c1392a9d9aedeccb
Uladzimir Al'khimovich <58719393+vladalh@users.noreply.github.com>
2022-02-10 22:49:53 +0300

    Add files via upload

---
+++ car_db.py
@@ -0,0 +1,38 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect("car.db")
+cursor = conn.cursor()
+
+# 1- creating table columns
+#cursor.execute("""CREATE TABLE car (
+#    name text,
+#    mpg float,
+#    cylinders integer,
+#    displacement float,
+#    horsepower float,
+#    weight float,
+#    acceleration float,
+#    model integer,
+#    country text)""")
+
+#print("Car table created")
+
+# 2 - Writing read data from csv-file
+#with open(f"cars.csv", "r", encoding="utf-8") as file:
+#    csv_reader = csv.reader(file, delimiter=";")
+#    columns = next(csv_reader)
+#    for line in csv_reader:
+#        cursor.execute("INSERT INTO car "
+#                       "(name, mpg, cylinders, displacement, horsepower, "
+#                       "weight, acceleration, model, country) VALUES (?, ?, ?, ?, ?, ?, ?, ? ,?)", line)
+
+
+cursor.execute("SELECT rowid, * FROM car")
+items = cursor.fetchall()
+for item in items:
+    print(item)
+
+conn.commit()
+
+conn.close()
